ec4fe3ceef883807fbf94ee543ad1e6207278932
CASA <CASA@DESKTOP-1F50NIJ>
2017-05-22 22:39:44 -0600

    CAMBIOS

---
+++ CARLOS/CARLOS/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CARLOS.Startup))]
+namespace CARLOS
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}

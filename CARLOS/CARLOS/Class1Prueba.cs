0dc96ec60215531783b07f283d2c76f456c32f2c
CASA <CASA@DESKTOP-1F50NIJ>
2017-05-22 22:48:42 -0600

    clases

---
+++ CARLOS/CARLOS/Class1Prueba.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CARLOS
+{
+    public class Class1Prueba
+    {
+       int  comunidad;
+    }
+}
